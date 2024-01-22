40f4d33984247670739f6dc869593815b0fafd0d
tuti2500 <155018947+tuti2500@users.noreply.github.com>
2024-01-22 10:52:51 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-alert('¡Bienvenida y bievenido a nuestro sitio web!');
-let nombre = "Lua";
-let edad = 25;
-let numeroDeVentas = 50;
-let saldoDisponible = 1000;
-alert("¡Error!Completa todos los campos");
-let mensajeDeError = '¡Error! Completa todos los campos';
-alert("mensajeDeError");
+//Variables
+let numeroSecreto = 2;
+let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
 
-let nombre = prompt('Digite su nombre');
-
-let edad = prompt ('Digite su edad');
-if (edad >=18) {
-    alert ('¡Puedes obtener tu licencia de conducir!');
+console.log(numeroUsuario);
+/*
+Este código realiza
+la comparación
+*/
+if (numeroUsuario == numeroSecreto) {
+    //Acertamos, fue verdadera la condición
+    alert(`Acertaste, el número es: ${numeroUsuario}`);
+} else {
+    //La condición no se cumplió
+    alert('Lo siento, no acertaste el número');
 }
